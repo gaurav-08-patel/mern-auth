fd604449b6b0028417874f2a640e3eee20d43cc5
gaurav-08-patel <gauravpatel.rounak@gmail.com>
2025-08-22 18:16:50 +0545

    create pages and added react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+
 function App() {
-    return <div className=" bg-amber-200">hello</div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sign-up" element={<Signup />} />
+                <Route path="/sign-in" element={<Signin />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
