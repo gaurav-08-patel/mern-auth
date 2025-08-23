c58fb3f87b009448b3881b041cb1a13026a6858e
gaurav-08-patel <gauravpatel.rounak@gmail.com>
2025-08-23 13:58:27 +0545

    created signup page UI

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,7 +4,7 @@ const Layout = ({ children }) => {
     return (
         <div>
             <Header />
-            <div>{children}</div>
+            <div className="max-w-[1100px] mx-auto min-h-[calc(100vh-64px)] p-1">{children}</div>
         </div>
     );
 };
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,46 @@
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-  return (
-    <Layout>Signup</Layout>
-  )
-}
+    return (
+        <Layout>
+            <div>
+                <h1 className="font-semibold text-center text-2xl my-4 tracking-[2px]">
+                    Sign Up
+                </h1>
+                <form className="flex flex-col gap-3 p-1 max-w-lg mx-auto">
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        className="bg-slate-200 p-2.5 rounded-lg"
+                        id="username"
+                    />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="bg-slate-200 p-2.5 rounded-lg"
+                        id="email"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className="bg-slate-200 p-2.5 rounded-lg"
+                        id="password"
+                    />
 
-export default Signup
+                    <button className=" bg-slate-600 text-white p-2.5 rounded-lg cursor-pointer">
+                        SIGN UP{" "}
+                    </button>
+                    <span className="text-sm">
+                        Have an account ?{" "}
+                        <Link to="/signin" className="text-blue-500">
+                            Sign in
+                        </Link>
+                    </span>
+                </form>
+            </div>
+        </Layout>
+    );
+};
+
+export default Signup;
