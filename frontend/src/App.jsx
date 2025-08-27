3fe5eacfe7a183057056830135f9d8aafa0b21c7
gaurav-08-patel <gauravpatel.rounak@gmail.com>
2025-08-27 20:29:55 +0545

    created  authContext and added persistance login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,42 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ToastContainer, Zoom } from "react-toastify";
+import { ToastContainer , Zoom} from "react-toastify";
+import { Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
+    let { authUser } = useAuthContext();
+
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/signin" element={<Signin />} />
+                <Route
+                    path="/"
+                    element={authUser ? <Home /> : <Navigate to={"/signin"} />}
+                />
+                <Route
+                    path="/signup"
+                    element={authUser ? <Navigate to={"/"} /> : <Signup />}
+                />
+                <Route
+                    path="/signin"
+                    element={authUser ? <Navigate to={"/"} /> : <Signin />}
+                />
                 <Route path="/about" element={<About />} />
-                <Route path="/profile" element={<Profile />} />
+                <Route
+                    path="/profile"
+                    element={
+                        authUser ? (
+                            <Profile to={"/"} />
+                        ) : (
+                            <Navigate to={"/signin"} />
+                        )
+                    }
+                />
             </Routes>
             <ToastContainer
                 position="top-center"
